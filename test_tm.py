9e5dd53d41143f495fc2e4546cf67855f659ab56
Spencer Kee <spencerkee7@gmail.com>
2016-11-08 23:35:10 -0500

    results and all pickles
    Former-commit-id: 1f4e32ea6328e8c8af12887fad042eabae47f3a3

--- test_tm.py
+++ test_tm.py
@@ -1,26 +1,20 @@
-from nltk.tokenize import RegexpTokenizer
-from stop_words import get_stop_words
-from nltk.stem.porter import PorterStemmer
-import pattern
-from gensim import corpora, models
+from gensim import corpora, models#
 import gensim
 import pickle
 from process_files import create_user_rating_history, create_movie_id_to_movie
-import sys
+import unicodedata
 
-def create_document_list():
-    movie_id_to_movie = create_movie_id_to_movie()#movie id: movie title
-    user_rating_history = create_user_rating_history() #user id: [(movie id,movie rating),etc]
+def create_movie_id_to_rating(user_rating_history,movie_id_to_movie):
     movie_id_to_rating = {} #movie id: [(user id, rating), etc]
     for user in user_rating_history:
-        # print ('user', user)
-        # print ('users', user_rating_history[user])
         for rating in user_rating_history[user]:
-            # print ('rating', rating)
             try:
                 movie_id_to_rating[rating[0]].append((user,rating[1]))
             except KeyError:
                 movie_id_to_rating[rating[0]] = [(user,rating[1])]
+    return movie_id_to_rating
+
+def create_document_list():
 
     doc_set = []
     movies = sorted(movie_id_to_rating.keys()) #not movie_id_to_movie because some movies have no ratings?
@@ -42,40 +36,60 @@ def create_document_list():
     # #for every user who rated the movie over 3:
     # #for every movie that user has rated over 3:
     # #add that movie to the document (1) times
-    return doc_set
+    return doc_set, movie_id_to_movie
+
+def replace_movie_id_with_name(string,movie_id_to_movie):
+    string = string.split('"')
+    string = [unicodedata.normalize('NFKD', i).encode('ascii','ignore') for i in string]
+    # print ('after_split',string)
+    for ind,value in enumerate(string):
+        if '.' not in value and len(value)>0:
+            string[ind] = movie_id_to_movie[value]
+    # print ('after replace',string)
+    return ''.join(string)
 
 def main(load=['texts','dictionary','corpus','ldamodel'],save=True):
-    # if 'texts' in load: #37 seconds
-    #     texts = pickle.load(open( "texts.p", "rb" ))
-    # else: #78 seconds
-    #     texts = create_document_list()
-    #     if save: pickle.dump(texts, open( "texts.p", "wb" ))
+    topic_num = 20
+    passes_num = 20
 
-    # if 'dictionary' in load: #2 seconds
-    #     dictionary = pickle.load(open( "dictionary.p", "rb" ))
-    # else:
-    #     # turn our tokenized documents into a id <-> term dictionary
-    #     dictionary = corpora.Dictionary(texts)
-    #     if save: pickle.dump(dictionary, open( "dictionary.p", "wb" ))
+    #1 second
+    movie_id_to_movie = create_movie_id_to_movie() #movie id: movie title
+    user_rating_history = create_user_rating_history() #user id: [(movie id,movie rating),etc]
+    movie_id_to_rating = create_movie_id_to_rating(user_rating_history,movie_id_to_movie)  #movie id: [(user id, rating), etc]
 
-    # if 'corpus' in load:
-    #     corpus = pickle.load(open( "corpus.p", "rb" ))
-    # else: #511 seconds
-    #     # convert tokenized documents into a document-term matrix
-    #     corpus = [dictionary.doc2bow(text) for text in texts]
-    #     if save: pickle.dump(corpus, open( "corpus.p", "wb" ))
+    if 'texts' in load: #37 seconds
+        texts = pickle.load(open( "texts.p", "rb" ))
+    else: #78 seconds
+        texts = create_document_list()
+        if save: pickle.dump(texts, open( "texts.p", "wb" ))
 
+    if 'dictionary' in load: #2 seconds
+        dictionary = pickle.load(open( "dictionary.p", "rb" ))
+    else:
+        # turn our tokenized documents into a id <-> term dictionary
+        dictionary = corpora.Dictionary(texts)
+        if save: pickle.dump(dictionary, open( "dictionary.p", "wb" ))
+
+    if 'corpus' in load: #172 seconds
+        corpus = pickle.load(open( "corpus.p", "rb" ))
+    else: #511 seconds
+        # convert tokenized documents into a document-term matrix
+        corpus = [dictionary.doc2bow(text) for text in texts]
+        if save: pickle.dump(corpus, open( "corpus.p", "wb" ))
+
+    lda_savename = "ldamodel" + str(topic_num) + "_" + str(passes_num) + ".p"
     if 'ldamodel' in load:
-        ldamodel = pickle.load(open( "ldamodel.p", "rb" ))
+        ldamodel = pickle.load(open(lda_savename, "rb" ))
     else:
         # generate LDA model
-        ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=20, id2word = dictionary, passes=20)
-        if save: pickle.dump(ldamodel, open( "ldamodel.p", "wb" ))
+        ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=topic_num, id2word = dictionary, passes=20)
+        if save: pickle.dump(ldamodel, open(lda_savename, "wb" ))
 
-    for i in ldamodel.print_topics(num_topics=20, num_words=8):
-        print (i)
+    topics = ldamodel.print_topics(num_topics=topic_num, num_words=6)
+    for topic in topics:
+        print (topic[0], replace_movie_id_with_name(topic[1],movie_id_to_movie))
 
 #num_topics=20, passes=20
 
 if __name__ == '__main__':
-    main(load=['texts','corpus', 'dictionary','ldamodel'],save=True)
+    main(load=['texts','dictionary', 'corpus'],save=True)
