4b398865aab048f5c7359f0d213779ded2fc4007
Spencer Kee <spencerkee7@gmail.com>
2016-11-08 21:56:24 -0500

    documentation and loading model
    Former-commit-id: 32f63e7a2ed5d8d46b489f7453f7e1e5e7d0cb7a

--- test_tm.py
+++ test_tm.py
@@ -6,6 +6,7 @@ from gensim import corpora, models
 import gensim
 import pickle
 from process_files import create_user_rating_history, create_movie_id_to_movie
+import sys
 
 def create_document_list():
     movie_id_to_movie = create_movie_id_to_movie()#movie id: movie title
@@ -41,71 +42,40 @@ def create_document_list():
     # #for every user who rated the movie over 3:
     # #for every movie that user has rated over 3:
     # #add that movie to the document (1) times
-    # for movie in sorted(movie_id_to_movie.keys()):#can be range(num_movies)
-    #     doc_string = ''
-    #     for 
-
     return doc_set
 
-# texts = create_document_list()
-# pickle.dump(texts, open( "texts.p", "wb" ))
+def main(load=['texts','dictionary','corpus','ldamodel'],save=True):
+    # if 'texts' in load: #37 seconds
+    #     texts = pickle.load(open( "texts.p", "rb" ))
+    # else: #78 seconds
+    #     texts = create_document_list()
+    #     if save: pickle.dump(texts, open( "texts.p", "wb" ))
 
-# tokenizer = RegexpTokenizer(r'\w+')
+    # if 'dictionary' in load: #2 seconds
+    #     dictionary = pickle.load(open( "dictionary.p", "rb" ))
+    # else:
+    #     # turn our tokenized documents into a id <-> term dictionary
+    #     dictionary = corpora.Dictionary(texts)
+    #     if save: pickle.dump(dictionary, open( "dictionary.p", "wb" ))
 
-# # create English stop words list
-# en_stop = get_stop_words('en')
+    # if 'corpus' in load:
+    #     corpus = pickle.load(open( "corpus.p", "rb" ))
+    # else: #511 seconds
+    #     # convert tokenized documents into a document-term matrix
+    #     corpus = [dictionary.doc2bow(text) for text in texts]
+    #     if save: pickle.dump(corpus, open( "corpus.p", "wb" ))
 
-# # Create p_stemmer of class PorterStemmer
-# p_stemmer = PorterStemmer()
+    if 'ldamodel' in load:
+        ldamodel = pickle.load(open( "ldamodel.p", "rb" ))
+    else:
+        # generate LDA model
+        ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=20, id2word = dictionary, passes=20)
+        if save: pickle.dump(ldamodel, open( "ldamodel.p", "wb" ))
 
-# # create sample documents
-# doc_a = "Brocolli is good to eat. My brother likes to eat good brocolli, but not my mother."
-# doc_b = "My mother spends a lot of time driving my brother around to baseball practice."
-# doc_c = "Some health experts suggest that driving may cause increased tension and blood pressure."
-# doc_d = "I often feel pressure to perform well at school, but my mother never seems to drive my brother to do better."
-# doc_e = "Health professionals say that brocolli is good for your health."
-
-# # compile sample documents into a list
-# doc_set = [doc_a, doc_b, doc_c, doc_d, doc_e]
-
-# # list for tokenized documents in loop
-# texts = []
-
-# # loop through document list
-# for i in doc_set:
-
-#     # clean and tokenize document string
-#     raw = i.lower()
-#     tokens = tokenizer.tokenize(raw)
-#     print("tokens: {}".format(tokens))
-
-#     # remove stop words from tokens
-#     stopped_tokens = [i for i in tokens if not i in en_stop]
-#     print("stopped_tokens: {}".format(stopped_tokens))
-
-#     # stem tokens
-#     stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
-#     print("stemmed_tokens: {}".format(stemmed_tokens))
-
-#     # add tokens to list
-#     texts.append(stemmed_tokens)
-#     break
-
-texts = pickle.load(open( "texts.p", "rb" ))
-
-# turn our tokenized documents into a id <-> term dictionary
-dictionary = corpora.Dictionary(texts)
-pickle.dump(ldamodel, open( "dictionary.p", "wb" ))
-
-# convert tokenized documents into a document-term matrix
-corpus = [dictionary.doc2bow(text) for text in texts]
-pickle.dump(ldamodel, open( "corpus.p", "wb" ))
-
-# generate LDA model
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=20, id2word = dictionary, passes=20)
-pickle.dump(ldamodel, open( "ldamodel.p", "wb" ))
-
-for i in ldamodel.print_topics(num_topics=20, num_words=8):
-    print (i)
+    for i in ldamodel.print_topics(num_topics=20, num_words=8):
+        print (i)
 
 #num_topics=20, passes=20
+
+if __name__ == '__main__':
+    main(load=['texts','corpus', 'dictionary','ldamodel'],save=True)
