d138358c423781827e72f1fd981b6ba642c2184d
Spencer Kee <spencerkee7@gmail.com>
2016-11-09 15:50:33 -0500

    1600
    Former-commit-id: 508a9becc1b8e25fe9731900f836e90e4aa4146c

--- test_tm.py
+++ test_tm.py
@@ -18,17 +18,13 @@ def create_movie_id_to_rating(user_rating_history,movie_id_to_movie):
 
 def create_document_list(movie_id_to_rating,user_rating_history):
     doc_set = []
-    movies = sorted(movie_id_to_rating.keys()) #not movie_id_to_movie because some movies have no ratings?
-    for movie in movies:#can be range(num_movies), for every movie
+    for user in user_rating_history.keys()[:1600]:
         doc_tags_list = []
-        for user_rating in movie_id_to_rating[movie]:
-            if float(user_rating[1]) >= 4:
-                for rated_movie in user_rating_history[user_rating[0]]:
-                    if rated_movie[0] == movie or (float(rated_movie[1]) >= 4): #don't want to have cyclical associations
-                        continue
-                    doc_tags_list.append(rated_movie[0])
-        # doc_tags_list = sorted(doc_tags_list, key=int) #possibly remove, bad for preformance and only in for visualization purposes
+        for movie_rating in user_rating_history[user]:
+            if float(movie_rating[1]) >= 4:
+                doc_tags_list.append(movie_rating[0])
         doc_set.append(doc_tags_list)
+
     # doc_set = []
     #for every movie:
     #for every user who rated the movie over x:
@@ -93,6 +89,7 @@ def main(load=['texts','dictionary','corpus','ldamodel'],save=True,topic_num=20,
             f.write(str(topic[0]) + ' ' + str(replace_movie_id_with_name(topic[1],movie_id_to_movie) + '\n'))
 
     print("total time: {} seconds".format(str(time.time()-start_time)))
+
 if __name__ == '__main__':
     if len(sys.argv) != 4:
         sys.exit('usage [topic_num passes_num save]')
