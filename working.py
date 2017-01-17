67660d95ef07ee2e4aaadf73d2a40867057a276c
Spencer Kee <skee13@simons-rock.edu>
2017-01-17 13:33:59 -0500

    Removed incorrect funciton call
    Former-commit-id: 5e6336147bcda02cd31d528df1211264dd0db9bb

--- working.py
+++ working.py
@@ -1,9 +1,8 @@
 import operator
 from collections import namedtuple
 
-from gensim.models import LdaModel
-from gensim.models import CoherenceModel
-from gensim.corpora import Dictionary
+
+from gensim import corpora, models, similarities
 
 import matplotlib.pyplot as plt
 
@@ -12,6 +11,7 @@ import pickle
 import time
 import unicodedata
 
+import os
 # %matplotlib inline
 
 def create_movie_id_to_rating(user_rating_history,movie_id_to_movie):
@@ -42,6 +42,7 @@ def create_document_list(movie_id_to_rating,user_rating_history,threshold=5):
                         continue
                     doc_tags_list.append(rated_movie[0])
         doc_set.append(doc_tags_list)
+
     return doc_set
 
 def range_list(start, limit, inc=1):
@@ -71,9 +72,9 @@ def evaluate_graph(dictionary, corpus, texts, start_val, limit, step=1):
     num_topics = start_val
     while num_topics < limit:
         print ('Current # of topics: ' + str(num_topics))
-        lm = LdaModel(corpus=corpus, num_topics=num_topics, id2word=dictionary)
+        lm = models.LdaModel(corpus=corpus, num_topics=num_topics, id2word=dictionary)
         # lm_list.append(lm)
-        cm = CoherenceModel(model=lm, texts=texts, dictionary=dictionary, coherence='c_v')
+        cm = models.CoherenceModel(model=lm, texts=texts, dictionary=dictionary, coherence='c_v')
         print (str((cm.get_coherence())))
         c_v.append(cm.get_coherence())
         num_topics += step
@@ -88,25 +89,50 @@ def evaluate_graph(dictionary, corpus, texts, start_val, limit, step=1):
     
     return lm_list
 
+def replace_movie_id_with_name(string,movie_id_to_movie):
+    string = string.split('"')
+    string = [unicodedata.normalize('NFKD', i).encode('ascii','ignore') for i in string]
+    for ind,value in enumerate(string):
+        if '.' not in value and len(value)>0:
+            string[ind] = movie_id_to_movie[value]
+    return ''.join(string)
+
 def main(name):
+    #utility dictionaries
     movie_id_to_movie = create_movie_id_to_movie() #movie id: movie title
-    user_rating_history = create_user_rating_history() #user id: [(movie id,movie rating),etc]
+    user_rating_history = create_user_rating_history() #user id: [(movie id,movie rating), etc]
     movie_id_to_rating = create_movie_id_to_rating(user_rating_history,movie_id_to_movie)  #movie id: [(user id, rating), etc]
 
+    #active variables
     texts = [i for i in create_document_list(movie_id_to_rating,user_rating_history) if len(i) >= 100]
-    dictionary = Dictionary(texts)
+    dictionary = corpora.Dictionary(texts)
     corpus = [dictionary.doc2bow(text) for text in texts]
 
-    lm_list = evaluate_graph(dictionary, corpus, texts, 10, 101, 10)
+    # saving active variables
+    # variables = [texts,dictionary,corpus]
+    # pickle.dump(variables,open('active_variables/' + name + '-variables.p', "wb"))
+    # texts, dictionary, corpus = pickle.load(open('active_variables/threshold_5-movies_all_small-len_100-variables.p', 'rb'))
+
+    # running and saving models
+    # ldamodel = models.ldamodel.LdaModel(corpus, num_topics=topic_num, id2word = dictionary, passes=10)
+    # topics = ldamodel.show_topics(num_topics=topic_num, num_words=6)
+    # ldamodel.save(fname=("active_variables/" + name))
+
+    ldamodel = models.ldamodel.LdaModel.load(fname=("active_variables/threshold_5-movies_all_small-len_100-topics_20-passes_20"))
+    ldamodel.update(corpus, passes=10)
+    ldamodel.save(fname=("active_variables/" + name))
+    topics = ldamodel.print_topics(num_topics=topic_num, num_words=6)
+    filename = 'current_results/' + name
+    with open(filename,'wb') as f:
+        for topic in topics:
+            f.write(str(topic[0]) + ' ' + str(replace_movie_id_with_name(topic[1],movie_id_to_movie) + '\n'))
+
+    # lm_list = evaluate_graph(dictionary, corpus, texts, 10, 101, 10)
     # lm_list_savename = 'current_results/' + name + '.p'
     # pickle.dump(lm_list,open(lm_list_savename, "wb"))
 
 if __name__ == '__main__':
     start_time = time.time()
-    main(name='10-100-step_10-large')
+    topic_num = 20
+    main(name='threshold_5-movies_all_small-len_100-topics_20-passes_30')
     print("total time: {} seconds".format(str(time.time()-start_time)))
-
-
-
-    # ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=topic_num, id2word = dictionary, passes=10)
-    # topics = ldamodel.print_topics(num_topics=topic_num, num_words=6)
